4c4b634bead759c713dd465a570c26e9c1b2bed2
liset29 <79374931315t@gmail.com>
2025-01-10 09:39:37 +0500

    added description of enpoints

--- service_x/app/routes.py
+++ service_x/app/routes.py
@@ -2,27 +2,47 @@ import uuid
 import asyncio
 from fastapi import HTTPException
 from faststream.rabbit.fastapi import RabbitRouter
+from starlette import status
 from service_x.app.shemas import TaskSchema, AllTasksResponse
 from service_x.config import start_task_queue, create_task_queue, request_queue, response_queue, RABBITMQ_URL
 
-router = RabbitRouter(RABBITMQ_URL)
+router = RabbitRouter(RABBITMQ_URL,prefix="/tasks", tags=["Tasks"])
 
 task_futures = {}
 
 
-@router.post("/create_tasks", response_model=dict)
+@router.post(
+    "/create_tasks",
+    response_model=dict,
+    description="Endpoint to create a task in Microservice Y",
+    response_description="Confirmation of task creation",
+    status_code=status.HTTP_201_CREATED,
+    response_model_by_alias=False,
+)
 async def create_task(number: str):
     await router.broker.publish({'number': number}, queue=create_task_queue)
     return {'status': 'created'}
 
-
-@router.post("/tasks/{id}/start", response_model=dict)
+@router.post(
+    "/tasks/{id}/start",
+    response_model=dict,
+    description="Endpoint to start a task by ID in Microservice Y",
+    response_description="Confirmation of task start",
+    status_code=status.HTTP_200_OK,
+    response_model_by_alias=False,
+)
 async def start_task(id: int):
     await router.broker.publish({"id": id}, queue=start_task_queue)
     return {"message": "Task started"}
 
-
-@router.get("/get_tasks", response_model=AllTasksResponse)
+@router.get(
+    "/get_tasks",
+    response_model=AllTasksResponse,
+    description="Endpoint to retrieve all tasks and their statuses from Microservice Y",
+    response_description="List of all tasks with their statuses",
+    status_code=status.HTTP_200_OK,
+    response_model_by_alias=False,
+)
 async def get_tasks():
     correlation_id = str(uuid.uuid4())
     future = asyncio.Future()
@@ -34,11 +54,16 @@ async def get_tasks():
     )
 
     response = await future
-
     return response
 
-
-@router.get('/get_task', response_model=TaskSchema)
+@router.get(
+    '/get_task',
+    response_model=TaskSchema,
+    description="Endpoint to retrieve a specific task by ID from Microservice Y",
+    response_description="Details of the requested task",
+    status_code=status.HTTP_200_OK,
+    response_model_by_alias=False,
+)
 async def get_task(id: int):
     correlation_id = str(uuid.uuid4())
     future = asyncio.Future()
--- service_x/config.py
+++ service_x/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 RABBITMQ_URL = os.getenv("RABBITMQ_URL")
-DATABASE_URL = os.getenv("DATABASE_URL")
 create_task_queue = os.getenv("CREATE_TASK_QUEUE")
 start_task_queue = os.getenv("START_TASK_QUEUE")
 request_queue = os.getenv("REQUEST_TASK_QUEUE")
