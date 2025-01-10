083f479c353a6a9ffe0f6d8196d7eabd3f7aafa2
liset29 <79374931315t@gmail.com>
2025-01-10 09:57:23 +0500

    added README.MD

--- service_y/app/db/database.py
+++ service_y/app/db/database.py
@@ -7,6 +7,5 @@ from service_y.app.db.models import Base
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 db_session = Session()
-# использовать const
 
 Base.metadata.create_all(bind=engine)
